af879feca3bb13c6bffb54faba2d1ffa7f1e1fd3
jayaramvulchi <jayaramarajuvulchi@gmail.com>
2025-01-10 15:53:01 +0530

    new test case

---
+++ arraysmethods/arrays-concat.js
@@ -0,0 +1,14 @@
+//This method used to merge tow or more arrays//
+//This method does not change the existing arrays but insted returns a new array//
+
+const array1 = ['a','b','c'];
+const array2 = ['d','6','f'];
+const array3 = array1.concat(array2);
+console.log(array3)
+
+
+const fruit = ['apple', 'banana'];
+const vegetables = ['carrot','spinach'];
+const dairy = ['milk', 'cheese'];
+const all = fruit.concat(vegetables,dairy);
+console.log(all);
